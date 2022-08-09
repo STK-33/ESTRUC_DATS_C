40b20911eb84668d04abee7974556c32b5cd8636
STK-33 <he.arriaga.a@edu.utc.mx>
2022-08-09 08:28:42 -0500

    contra

--- Cajero_33/Cajero_33.cpp
+++ Cajero_33/Cajero_33.cpp
@@ -3,6 +3,12 @@
 #include <cstdlib>
 #include <cmath>
 #include <stdlib.h>
+#include <vector>
+#include <conio.h> // getch util
+
+#define ENTER 13
+#define BACKSPACE 8
+#define INTENTOS 3
 using namespace std;
 
 //funciones 
@@ -58,35 +64,105 @@ void menuContrasena()
 	
   int opcion;
     bool repetir = true;
-    system("cls");
-    // nip correcto
-    const string nip = "abc123";
-    cout << endl;
-    cout << "------------------------  \n";
-    cout << "  INGRESA TU NIP \n";
-    cout << "------------------------  \n";
-    cout << endl;
     
-    string contra = "";
-    cin >> contra;
-    if (contra == nip)
-    {
-         // Si llegamos hasta aquí, todo bien por el momento
-    	cout << "\nBienvenido al $$$$$$\n";
-    	 cout << endl;
-    	system("pause");
-    }else
-            {
-            	cout << endl;
-		cout << "NIP incorrecto vuelve a intentar";
-		 cout << endl;
-        // pausamos un momento 
-         system("pause");
-         menuContrasena();
-            }
-   
-      
+    system("cls"); //limpiamos la consola
+    
+   vector<string> clientes;
+    vector<string> contrasenas;
+
+    // para añadir clientes
+    clientes.push_back("admin");
+    // para añadir contraseña alfanumerica
+    contrasenas.push_back("Abc123");
+    
+    string cliente, nip;
+
+    int contador = 0;
+    bool ingresar = false;
+
     do
+    {
+        system("cls");
+        cout << endl;
+        cout << "\t\t\tLOGIN DE CLIENTES" << endl;
+        cout << "\t\t\t------------------" << endl;
+        cout << endl;
+        cout << "\nNombre del Cliente: ";
+        cout << endl;
+    
+        cin >> cliente;
+
+        char caracter;
+
+        cout << "\nNIP : ";
+        
+        caracter = getch();  //para tapar la contraseña con ****** 
+
+        nip = "";
+
+        while (caracter != ENTER)
+        {
+
+            if (caracter != BACKSPACE)
+            {
+                nip.push_back(caracter);
+                cout << "*";
+            }
+            else
+            {
+                if (nip.length() > 0)
+                {
+                    cout << "\b \b";
+                    nip = nip.substr(0, nip.length() - 1);
+                }
+            }
+
+        caracter = getch();
+        }
+
+        for (int i = 0; i < clientes.size(); i++)
+        {
+            if (clientes[i] == cliente && contrasenas[i] == nip)
+            {
+                ingresar = true;
+                break;
+            }
+        }
+
+        if (!ingresar)
+        {
+        	system("cls");
+            cout << "\n\n\tEl Nombre de clinete y/o NIP son incorrectos----(Solo cuentas con 3 intentos)" << endl;
+            cout << endl;
+            cin.get();
+            contador++;
+            system("pause"); 
+        }
+
+    } while (ingresar == false && contador < INTENTOS);
+
+    if (ingresar == false)
+    {
+    	system("cls");
+        cout << "\n\tNumero maximo de intentos. " << endl;
+         cout << "\n\tCAJERO BLOQUEADO --- BUSCA A UN ASESOR . " << endl;
+          cout << "\n\t------------------  -  ------------------" << endl;
+          cout << "\n\t----------------  -----  ----------------" << endl;
+          cout << "\n\t--------------  ---   ---  --------------" << endl;
+          cout << "\n\t------------  -------------  ------------" << endl;
+          cout << "\n\t---------  --------   -------  ----------" << endl;
+          cout << "\n\t-------  ----------   ---------  --------" << endl;
+          cout << "\n\t-----  ------------   -----------  ------" << endl;
+          cout << "\n\t-----------------------------------------" << endl;
+    }
+    else
+    {
+        cout << "\n\n\tAcceso Correcto" << endl;
+          system("pause"); 
+         
+        //Aquí empieza el menu del sistema bancario ..................................................................
+        
+         do
     {
         system("cls");
 
@@ -168,13 +244,21 @@ void menuContrasena()
 		 
 
     }while (repetir);
-    
+ 
+        
+    }
+
+    cin.get();
+
+      
+      
 }
 
 void menuUltMov()
 {
 
-cout<<"\n\tEn proceso y mantenimiento ..!"<<endl;
+ cout<<"\n\tEn proceso ya csi se acaba !"<<endl;
+    
 
 }
    
